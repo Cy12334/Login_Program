8101abb6a0d0bac7d747a5e745264b735c6a13e3
Cy12334 <165625761+Cy12334@users.noreply.github.com>
2024-04-03 20:29:55 +0800

    Add files via upload

---
+++ Login_Program/Login_Program/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SuperLola
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int attempts = 3;
+
+            while (attempts > 0)
+
+            {
+                Console.Write("Enter your username: ");
+                string username = Console.ReadLine();
+
+                if (username == "Admin")
+                {
+                    Console.Write("Enter your password: ");
+                    string password = Console.ReadLine();
+
+                    if (password == "Admin1234!")
+                    {
+                        Console.WriteLine(" login susccessful");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("WRONG PASSWORD");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("WRONG USERNAME");
+                }
+                attempts--;
+                Console.WriteLine($"only {attempts} attemps.");
+            }
+            Console.WriteLine("no more attempts for you");
+        }
+
+    }
+
+}
